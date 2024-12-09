cf2ccef2aea6c700e7d851d7ac7b5244faf28cf8
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-09 13:23:52 +0530

    Bug fix db connection file

--- server/config/connectDB.js
+++ server/config/connectDB.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-if(process.env.MONGODB_URI) {
+if(!process.env.MONGODB_URI) {
     throw new Error("Please provide MONGODB_URI in the .env file");
 }
 
