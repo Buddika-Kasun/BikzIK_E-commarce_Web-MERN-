ebb5237badb0b76189ea38e194ff8b29dfa9f355
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-09 13:10:09 +0530

    Mongo DB connection file

---
+++ server/config/connectDB.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+if(process.env.MONGODB_URI) {
+    throw new Error("Please provide MONGODB_URI in the .env file");
+}
+
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("MongoDB connected");
+    }
+    catch (err) {
+        console.log("MongoDB connect error",err);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
