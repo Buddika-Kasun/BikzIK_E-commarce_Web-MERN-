3e7151120cd46a1ce3ac272a28c7956a96f6fbc5
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-09 17:44:46 +0530

    Verify Email Template

---
+++ server/utils/verifiyEmailTemplate.js
@@ -0,0 +1,11 @@
+const verifyEmailTemplate = (name, url) => {
+    return `
+        <p>Dear ${name}</p>
+        <p>Thank you for register Binkeyit.</p>
+        <a href=${url} style="color:white;background-color:blue;margin-top:10px">
+            Verify Email
+        </a>
+    `;
+};
+
+export default verifyEmailTemplate;
