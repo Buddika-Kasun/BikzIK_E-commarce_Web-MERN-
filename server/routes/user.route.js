f9702c33d29dd82aeb903b38dcf72046ea8d8860
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-11 16:02:54 +0530

    Add logout route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
-import { loginUserController, registerUserController, verifyEmailController } from '../controllers/user.controller.js';
+import { 
+    loginUserController,
+    logoutUserController,
+    registerUserController,
+    verifyEmailController,
+} from '../controllers/user.controller.js';
+import auth from '../middlewares/auth.js';
 
 const userRouter = new Router();
 
 userRouter.post('/register', registerUserController);
 userRouter.post('/verify-email', verifyEmailController);
 userRouter.post('/login', loginUserController);
+userRouter.get('/logout', auth, logoutUserController)
 
 export default userRouter;
