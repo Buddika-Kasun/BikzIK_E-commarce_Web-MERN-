c4d25e99822f65e1c1dcdc7d715e6c9b86a11257
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-12 00:43:55 +0530

    Add update avatar route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,9 +3,11 @@ import {
     loginUserController,
     logoutUserController,
     registerUserController,
+    uploadAvatarController,
     verifyEmailController,
 } from '../controllers/user.controller.js';
 import auth from '../middlewares/auth.js';
+import upload from '../middlewares/multer.js';
 
 const userRouter = new Router();
 
@@ -13,5 +15,6 @@ userRouter.post('/register', registerUserController);
 userRouter.post('/verify-email', verifyEmailController);
 userRouter.post('/login', loginUserController);
 userRouter.get('/logout', auth, logoutUserController);
+userRouter.put('/upload-avatar', auth, upload.single('avatar'), uploadAvatarController);
 
 export default userRouter;
