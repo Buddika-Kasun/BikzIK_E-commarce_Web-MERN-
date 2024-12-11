f9329b1473de30b592b0ee3a56babe1d4e37eb39
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-11 16:02:04 +0530

    Auth middleware file

---
+++ server/middlewares/auth.js
@@ -0,0 +1,40 @@
+import jwt from 'jsonwebtoken';
+
+const auth = (req, res, next) => {
+    try{
+        const token = req.cookies.accessToken || req.header?.authorization?.split(" ")[1]; // ["aaaa","token"]
+
+        if(!token) {
+            return res.status(401).json({
+                message: 'Provide token to access',
+                error: true,
+                success: false,
+            });
+        }
+
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        if(!decoded){
+            return res.status(401).json({
+                message: 'Unauthorized access',
+                error: true,
+                success: false,
+            });
+        }
+
+        req.userId = decoded.id;
+
+        next();
+
+    }
+    catch(err){
+        console.error(err);
+        return res.status(500).json({
+            message: err.message || err,
+            error: true,
+            success: false,
+        });
+    }
+};
+
+export default auth;
