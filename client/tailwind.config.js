e44dae924137e335615709cc942254586ad7e203
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-13 00:08:21 +0530

    Add color & container theme to tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        //lg: '3rem',
+        //xl: '4rem',
+        //'2xl': '5rem',
+      },
+    },
+    extend: {
+      colors: {
+        'primary-200': '#ffb900',
+        'primary-100': '#ffc929',
+        'secondary-200': '#00b050',
+        'secondary-100': '#0b1a78',
+      },
+    },
   },
   plugins: [],
 }
