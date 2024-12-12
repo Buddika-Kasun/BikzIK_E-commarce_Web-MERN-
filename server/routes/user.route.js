3cf5302f03aae2cbd72c19287324fc2467b2eefd
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-12 12:52:35 +0530

    Add update profile route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
+import auth from '../middlewares/auth.js';
+import upload from '../middlewares/multer.js';
 import { 
     loginUserController,
     logoutUserController,
     registerUserController,
+    updateProfileController,
     uploadAvatarController,
     verifyEmailController,
 } from '../controllers/user.controller.js';
-import auth from '../middlewares/auth.js';
-import upload from '../middlewares/multer.js';
 
 const userRouter = new Router();
 
@@ -16,5 +17,6 @@ userRouter.post('/verify-email', verifyEmailController);
 userRouter.post('/login', loginUserController);
 userRouter.get('/logout', auth, logoutUserController);
 userRouter.put('/upload-avatar', auth, upload.single('avatar'), uploadAvatarController);
+userRouter.put('/update-user', auth, updateProfileController);
 
 export default userRouter;
