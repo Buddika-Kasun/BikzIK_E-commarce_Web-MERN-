32df0a6fa92a96f0d113f3a64f82dd47115c47b7
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-14 20:38:06 +0530

    Register page modify

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -72,7 +72,7 @@ const RegisterPage = () => {
 
   return (
     <section className="w-full">
-      <div className="bg-white my-4 p-7 rounded w-full max-w-lg mx-auto">
+      <div className="bg-white my-4 p-7 rounded w-full max-w-lg mx-auto shadow-lg">
         <p className="text-lg w-full text-center font-bold">Welcome to BikzIK</p>
 
         {/* Register form */}
