4757e20d753ca89c75e81ed71c4004342d223f7d
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-18 11:39:03 +0530

    Fetch user details file

---
+++ client/src/utils/fetchUserDetails.js
@@ -0,0 +1,18 @@
+import Axios from './Axios';
+import SummaryApi from '../common/SummaryApi';
+
+const fetchUserDetails = async() => {
+    try {
+
+        const response = await Axios({
+            ...SummaryApi.user_details,
+        });
+
+        return response.data;
+
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export default fetchUserDetails;
