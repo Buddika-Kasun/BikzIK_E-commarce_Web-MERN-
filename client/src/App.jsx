1a400291b47b889f2638b2390306d018eaec628e
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-18 11:38:32 +0530

    Fetch logged user details and save it to redux state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,24 @@ import './App.css'
 import Header from './components/Header';
 import Footer from './components/Footer';
 import toast, { Toaster } from 'react-hot-toast';
+import fetchUserDetails from './utils/fetchUserDetails';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setUser } from './store/userSlice';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  const fetchUser = async() => {
+    const userData = await fetchUserDetails();
+
+    dispatch(setUser(userData.data));
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
   return (
     <>
