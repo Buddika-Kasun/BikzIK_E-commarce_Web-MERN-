1d001e9f6d3e23d021a9d68fb1035a81197f329e
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-18 10:43:49 +0530

    Add redux provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,13 @@ import './index.css'
 // import App from './App.jsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/route.jsx';
+import { Provider } from 'react-redux';
+import { store } from './store/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>  {/* if this is comment or remove then render only 1 time, now render 2 time for dev purpose */}
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
