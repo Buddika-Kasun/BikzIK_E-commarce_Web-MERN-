27b98457694e652b19870edca0b00545dfeb26af
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-24 00:10:02 +0530

    Add upload avatar api url

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -32,6 +32,10 @@ const SummaryApi = {
     logout: {
         url: '/api/user/logout',
         method: 'GET',
+    },
+    upload_avatar: {
+        url: 'api/user/upload-avatar',
+        method: 'PUT',
     }
 };
 
