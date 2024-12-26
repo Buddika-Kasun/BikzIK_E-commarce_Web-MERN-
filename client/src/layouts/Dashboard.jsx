ff99f40b0e1cfaa92964c1f824c2c252e4c2aef9
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-26 22:53:14 +0530

    Check user login or not in dashboard and redirect to login page

--- client/src/layouts/Dashboard.jsx
+++ client/src/layouts/Dashboard.jsx
@@ -1,18 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import UserMenu from "../components/UserMenu";
-
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Dashboard = () => {
+
+    const user = useSelector(state => state.user);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(user._id === "") {
+            navigate("/login");
+        }
+    }, [user._id, navigate]);
+
     return (
         <section className="">
             <div className="container mx-auto p-3 grid lg:grid-cols-[250px,1fr]">
                 {/* Left Menu */}
-                <div className="p-4 sticky top-20 overflow-auto hidden lg:block border-r-2 border-r-slate-200 min-h-[77vh]">
+                <div className="p-4 sticky top-20 overflow-auto hidden lg:block border-r-2 border-r-slate-200 min-h-[73vh]">
                     <UserMenu />
                 </div>
 
                 {/* Right Content */}
-                <div className="p-3">
+                <div className="">
                     <Outlet />
                 </div>
             </div>
