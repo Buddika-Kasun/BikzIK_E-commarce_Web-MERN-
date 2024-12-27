b0b6d7ad5f4be4d5e80753616129eec77eeb8e08
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-27 11:35:56 +0530

    Upload category component complete

---
+++ client/src/components/UploadCategoryModel.jsx
@@ -0,0 +1,193 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { IoClose } from "react-icons/io5";
+import uploadImage from "../utils/UploadImage";
+import { FaCloudUploadAlt, FaEdit } from "react-icons/fa";
+import Axios from "../utils/Axios";
+import SummaryApi from "../common/SummaryApi";
+import AxiosToastError from "../utils/AxiosToastError";
+
+const UploadCategoryModel = ({close}) => {
+
+    const [data, setData] = useState({
+        name: "",
+        image: ""
+    });
+
+    const [previewUrl, setPreviewUrl] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const handleImage = (e) => {
+        const imageFile = e.target.files[0];
+
+        if(!imageFile){
+            toast.error("Please select an avatar!");
+            return;
+        }
+
+        // Allowed image types
+        const allowedTypes = ["image/jpeg", "image/png", "image/gif", "image/webp"];
+
+        if (!allowedTypes.includes(imageFile.type)) {
+            toast.error("Invalid file type! Please upload a JPEG, PNG, GIF, or WEBP image.");
+            return;
+        }
+
+        setData((prevData) => ({
+            ...prevData,
+            image: imageFile,
+        }));
+        
+        setPreviewUrl(URL.createObjectURL(imageFile));
+
+    };
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Check if all fields are filled
+        if (!data.name || !data.image) {
+            toast.error("Please fill all required fields!");
+            return;
+        }
+
+        try{
+            setLoading(true);
+
+            const imgResponse = await uploadImage(data.image);
+    
+            const uploadedImageUrl = imgResponse.data.data.url;
+    
+            const payload = {
+                name: data.name,
+                image: uploadedImageUrl
+            };
+
+            const response = await Axios({
+                ...SummaryApi.add_category,
+                data: payload
+            });
+
+            if(response.data.success) {
+                toast.success("Category added successfully!");
+                setData({
+                    name: "",
+                    image: ""
+                });
+                close();
+            }
+
+        }
+        catch(error){
+            console.log(error);
+            AxiosToastError(error);
+        }
+        finally{
+            setLoading(false);
+        }
+
+    }
+
+    return (
+        <section className="fixed top-0 right-0 bottom-0 left-0 bg-neutral-900 bg-opacity-60 p-4 flex items-center justify-center z-50">
+            <div className="bg-white max-w-4xl w-full p-4 rounded-md">
+                <div className="flex items-center justify-center">
+                    <h2 className="font-semibold">Category</h2>
+                    <button
+                        className="w-fit ml-auto hover:text-red-400"
+                        onClick={close}
+                    >
+                        <IoClose size={25} />
+                    </button>
+                </div>
+                <form className="grid gap-2 my-3" onSubmit={handleSubmit}>
+                    <div className="grid gap-1">
+                        <label htmlFor="name" className="text-sm">Name : </label>
+                        <input
+                            id="name"
+                            name="name"
+                            type="text"
+                            value={data.name}
+                            onChange={(e) => setData({...data, name: e.target.value })}
+                            placeholder="Enter category name"
+                            className="py-1 px-4 outline-none bg-slate-300 bg-opacity-40 focus-within:bg-opacity-60 border border-slate-400 focus-within:border-slate-500 rounded-md text-neutral-500"
+                            required
+                        />
+                    </div>
+                    <div className="grid gap-1">
+                        <p className="text-sm">Image : </p>
+                        <div className="flex gap-4 flex-col lg:flex-row items-center">
+                            <div className="bg-slate-300 border bg-opacity-40 border-slate-400 w-full lg:w-36 aspect-square rounded-md flex items-center justify-center overflow-hidden relative">
+                                { 
+                                    previewUrl ? (
+                                        <img src={previewUrl} className="w-full h-full"/>
+                                    ) : (
+                                        data.image ? (
+                                            <img
+                                                src={data.image} 
+                                                className="w-full h-full object-scale-down"
+                                            />
+                                        ) : (
+                                            <label htmlFor="uploadCategoryImage" className="w-full h-full">
+                                                <div
+                                                    className={`${data.name ? 'cursor-pointer opacity-40': 'opacity-20'} w-full h-full flex items-center justify-center`}
+                                                >
+                                                    <FaCloudUploadAlt size={50} />
+                                                     
+                                                </div>
+                                                <input
+                                                    id="uploadCategoryImage"
+                                                    type="file"
+                                                    accept="image/*"
+                                                    hidden
+                                                    className="hidden"
+                                                    disabled={!data.name}
+                                                    onChange={handleImage}
+                                                />
+                                            </label>
+                                        )
+                                    )
+                                }
+                                {
+                                    (previewUrl || data.img) && (
+                                        <label htmlFor="editImage">
+                                            <div
+                                                className={`absolute bottom-0 right-0 bg-white bg-opacity-60 flex p-1 rounded-tl-md ${data.name ? 'opacity-100 cursor-pointer' : 'opacity-50'}`}
+                                            >
+                                                <FaEdit />
+                                            </div>
+                                            <input
+                                                id="editImage"
+                                                type="file"
+                                                accept="image/*"
+                                                hidden
+                                                className="hidden"
+                                                disabled={!data.name}
+                                                onChange={handleImage}
+                                            />
+                                        </label>
+                                    )
+                                }
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <button
+                        disabled={!data.name || !data.image}
+                        className={`rounded-md py-1 mt-4 ${(data.name && data.image) ? 'bg-green-500 text-white hover:bg-green-600 cursor-pointer' : 'bg-slate-300'} ${loading ? 'cursor-auto' : 'cursor-default' }`}
+                    >
+                        {
+                            loading ? (
+                                'Adding...'
+                            ) : (
+                                'Add Category'
+                            )
+                        }
+                    </button>
+                </form>
+            </div>
+        </section>
+    );
+}
+
+export default UploadCategoryModel;
