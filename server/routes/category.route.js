dfcb258d20520f3d32839eb42d44a30933571307
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-28 12:41:30 +0530

    Add get all category route

--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import auth from '../middlewares/auth.js';
 import {
-    addCategoryController
+    addCategoryController,
+    getAllCategoriesController
 } from '../controllers/category.controller.js';
 
 const categoryRouter = new Router();
 
 categoryRouter.post('/add', auth, addCategoryController);
+categoryRouter.get('/get', getAllCategoriesController);
 
 export default categoryRouter;
