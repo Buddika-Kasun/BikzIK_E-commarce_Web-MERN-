1f4b51adb5ef0c8fb448c2f664cb0d3e64b2a37a
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-31 12:14:25 +0530

    Add update category route

--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import auth from '../middlewares/auth.js';
 import {
     addCategoryController,
-    getAllCategoriesController
+    getAllCategoriesController,
+    updateCategoryController
 } from '../controllers/category.controller.js';
 
 const categoryRouter = new Router();
 
 categoryRouter.post('/add', auth, addCategoryController);
 categoryRouter.get('/get', getAllCategoriesController);
+categoryRouter.put('/update', auth, updateCategoryController);
 
 export default categoryRouter;
