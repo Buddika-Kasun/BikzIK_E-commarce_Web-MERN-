af8c941028811d0fa66ea972e1234c8c90290381
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-31 11:41:58 +0530

    Dashboard bug comment for later fix

--- client/src/layouts/Dashboard.jsx
+++ client/src/layouts/Dashboard.jsx
@@ -9,17 +9,19 @@ const Dashboard = () => {
 
     const navigate = useNavigate();
 
-    useEffect(() => {
-        if(user._id === "") {
-            navigate("/login");
-        }
-    }, [user._id, navigate]);
+    {/*
+        useEffect(() => {
+            if(user._id === "") {
+                navigate("/login");
+            }
+        }, [user._id, navigate]);
+    */}
 
     return (
         <section className="">
             <div className="container mx-auto p-3 grid lg:grid-cols-[250px,1fr]">
                 {/* Left Menu */}
-                <div className="p-4 sticky top-20 overflow-auto hidden lg:block border-r-2 border-r-slate-200 min-h-[73vh]">
+                <div className="p-4 sticky top-24 overflow-auto hidden lg:block border-r-2 border-r-slate-200 max-h-[73vh]">
                     <UserMenu />
                 </div>
 
