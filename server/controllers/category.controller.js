cc553a9ece5a9728c4200e81d958c83e2bc8da15
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-01 13:42:24 +0530

    Add delete category controller

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -1,4 +1,6 @@
 import CategoryModel from '../models/category.model.js';
+import ProductModel from '../models/product.model.js';
+import SubCategoryModel from '../models/subCategory.model.js';
 
 // Add new category controller
 export const addCategoryController = async(req, res) => {
@@ -106,3 +108,50 @@ export const updateCategoryController = async(req, res) => {
         });
     }
 }
+
+// Delete category controller
+export const deleteCategoryController = async(req, res) => {
+    try{
+
+        const {categoryId} = req.body;
+
+        const dependSubCategory = await SubCategoryModel.find({
+            category: {
+                "$in": [categoryId]
+            }
+        }).countDocuments();
+        
+        const dependProduct = await ProductModel.find({
+            category: {
+                "$in": [categoryId]
+            }
+        }).countDocuments();
+        
+        if (dependProduct > 0 || dependProduct > 0) {
+            return res.status(400).json({
+                message: "Category is already used can't be delete",
+                error: true,
+                success: false,
+            });
+        }
+        
+        const deleteCategory = await CategoryModel.deleteOne({
+            _id: categoryId
+        });
+        
+        return res.json({
+            message: "Category deleted successfully",
+            error: false,
+            success: true,
+            data: deleteCategory,
+        });
+
+    }
+    catch(err){
+        return res.status(500).json({
+            message: err.message || err,
+            error: true,
+            success: false,
+        });
+    }
+}; 
