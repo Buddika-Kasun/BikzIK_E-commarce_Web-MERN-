f488afbe2eddf131a4396275ea6d7df085d16e97
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-01 14:59:02 +0530

    Logo component

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,12 @@
+
+
+const Logo = () => {
+    return (
+        <div className="text-4xl font-semibold">
+            <span className="text-green-500">Bikz</span>
+            Ik
+        </div>
+    );
+}
+
+export default Logo;
