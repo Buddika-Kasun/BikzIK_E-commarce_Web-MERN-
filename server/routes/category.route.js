b9ced855a0169c1e07ee56878026c381f5f04d60
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-01 13:42:39 +0530

    Add delete category route

--- server/routes/category.route.js
+++ server/routes/category.route.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import auth from '../middlewares/auth.js';
 import {
     addCategoryController,
+    deleteCategoryController,
     getAllCategoriesController,
     updateCategoryController
 } from '../controllers/category.controller.js';
@@ -11,5 +12,6 @@ const categoryRouter = new Router();
 categoryRouter.post('/add', auth, addCategoryController);
 categoryRouter.get('/get', getAllCategoriesController);
 categoryRouter.put('/update', auth, updateCategoryController);
+categoryRouter.delete('/delete', auth, deleteCategoryController);
 
 export default categoryRouter;
