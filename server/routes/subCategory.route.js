b40cdfe004c8b7ec856b428b6db144811fc2ada7
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-03 11:03:15 +0530

    Add update sub category route

--- server/routes/subCategory.route.js
+++ server/routes/subCategory.route.js
@@ -2,12 +2,14 @@ import { Router } from 'express';
 import auth from '../middlewares/auth.js';
 import {
     addSubCategoryController,
-    getAllSubCategoriesController
+    getAllSubCategoriesController,
+    updateSubCategoryController
 } from '../controllers/subCategory.controller.js';
 
 const subCategoryRouter = Router();
 
 subCategoryRouter.post('/add', auth, addSubCategoryController);
 subCategoryRouter.get('/get', getAllSubCategoriesController);
+subCategoryRouter.put('/update', updateSubCategoryController);
 
 export default subCategoryRouter;
