7282b2a86346edf8d06d6b12febce09059304248
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-03 11:02:52 +0530

    Add update sub category controller

--- server/controllers/subCategory.controller.js
+++ server/controllers/subCategory.controller.js
@@ -77,3 +77,41 @@ export const getAllSubCategoriesController = async(req, res) => {
         });
     }
 };
+
+// Update sub category controller
+export const updateSubCategoryController = async(req, res) => {
+    try{
+
+        const { subCategoryId, name, image, category } = req.body;
+
+        if(!subCategoryId && !name && !image && !category[0]) {
+            return res.status(400).json({
+                message: "Please provide all required fields",
+                error: true,
+                success: false,
+            });
+        }
+
+        const updateSubCategory = await SubCategoryModel.updateOne({
+            _id: subCategoryId
+        }, {
+            name,
+            image,
+            category,
+        });
+
+        return res.status(200).json({
+            message: "Subcategory updated successfully",
+            error: false,
+            success: true,
+        });
+
+    }
+    catch(err){
+        return res.status(500).json({
+            message: err.message || err,
+            error: true,
+            success: false,
+        });
+    }
+};
