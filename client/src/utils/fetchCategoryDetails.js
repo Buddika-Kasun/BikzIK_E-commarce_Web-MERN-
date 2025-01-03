bc17a157c3312d757b3a3306ed49ba893174b6d1
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-03 11:22:44 +0530

    Fetch category details file

---
+++ client/src/utils/fetchCategoryDetails.js
@@ -0,0 +1,20 @@
+import SummaryApi from "../common/SummaryApi";
+import Axios from "./Axios";
+
+const fetchCategoryDetails = async() => {
+    try{
+
+        const response = await Axios({
+            ...SummaryApi.get_category,
+        });
+
+        if (response.data.success) {
+            return response.data;
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+  }
+
+  export default fetchCategoryDetails;
