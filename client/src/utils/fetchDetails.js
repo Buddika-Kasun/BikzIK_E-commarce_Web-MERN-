9d4d90329b609af8cb78dc02de3575949134c2ec
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-04 13:27:36 +0530

    Fetch details function

---
+++ client/src/utils/fetchDetails.js
@@ -0,0 +1,20 @@
+import SummaryApi from "../common/SummaryApi";
+import Axios from "./Axios";
+
+const fetchDetails = async({url}) => {
+    try{
+
+        const response = await Axios({
+            ...SummaryApi[url],
+        });
+
+        if (response.data.success) {
+            return response.data;
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+  }
+
+  export default fetchDetails;
