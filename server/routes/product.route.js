351afc02631c3fa8ff3b78fa739a639a5a055628
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-05 19:17:20 +0530

    Add product get route

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import auth from '../middlewares/auth.js';
 import {
-    addProductController
+    addProductController,
+    getProductsController
 } from '../controllers/product.controller.js';
 
 const productRouter = new Router();
 
 productRouter.post('/add', auth, addProductController);
+productRouter.post('/get', getProductsController);
 
 export default productRouter;
