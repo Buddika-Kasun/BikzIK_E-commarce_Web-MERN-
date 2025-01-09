00d43c3e7404d9b4490cafd4a9ad584eebd70a18
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-09 12:25:46 +0530

    Add scrolling to category wise product component

--- client/src/components/CategoryWiseProduct.jsx
+++ client/src/components/CategoryWiseProduct.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import AxiosToastError from '../utils/AxiosToastError';
 import Axios from '../utils/Axios';
@@ -12,6 +12,7 @@ const CategoryWiseProduct = ({id, name}) => {
 
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
+    const containerRef = useRef();
 
     const fetchCategoryWiseProduct = async() => {
         try {
@@ -40,6 +41,14 @@ const CategoryWiseProduct = ({id, name}) => {
         fetchCategoryWiseProduct();
     }, []);
 
+    const handleScrollRight = () => {
+        containerRef.current.scrollLeft += 200;
+    };
+
+    const handleScrollLeft = () => {
+        containerRef.current.scrollLeft -= 200;
+    };
+
     const loadingCardNumber = new Array(6).fill(null); 
 
     return (
@@ -48,7 +57,7 @@ const CategoryWiseProduct = ({id, name}) => {
                 <h1 className='font-semibold text-lg md:text-xl'>{name}</h1>
                 <Link to='' className='text-green-500 hover:text-green-400'>See All</Link>
             </div>
-            <div className="flex items-center gap-4 overflow-auto px-4">
+            <div className="flex items-center gap-4 overflow-x-scroll lg:overflow-hidden px-4 pb-2 scroll-smooth" ref={containerRef}>
                 {
                     loading &&
                     loadingCardNumber.map((_,index) => {
@@ -62,11 +71,17 @@ const CategoryWiseProduct = ({id, name}) => {
                     })
                 }
 
-                <div className="absolute w-full px-6 mx-auto left-0 right-0 flex justify-between">
-                    <button className="">
+                <div className="absolute w-full px-6 mx-auto left-0 right-0 justify-between hidden lg:flex">
+                    <button
+                        className="bg-white hover:bg-gray-100 p-2 z-10 text-lg rounded-full shadow-lg"
+                        onClick={handleScrollLeft}
+                    >
                         <FaChevronLeft />
                     </button>
-                    <button className="">
+                    <button
+                        className="bg-white hover:bg-gray-100 p-2 z-10 text-lg rounded-full shadow-lg"
+                        onClick={handleScrollRight}
+                    >
                         <FaChevronRight />
                     </button>
                 </div>
