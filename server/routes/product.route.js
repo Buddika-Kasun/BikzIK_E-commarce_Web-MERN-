81e198b90d92f6494124eb05f999463d40bc42de
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-11 13:44:33 +0530

    Add product delete route

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import auth from '../middlewares/auth.js';
 import {
     addProductController,
+    deleteProductController,
     getProductByIdController,
     getProductsByCategoryAndSubcategoryController,
     getProductsByCategoryController,
@@ -18,5 +19,6 @@ productRouter.post('/get-by-category', getProductsByCategoryController);
 productRouter.post('/get-by-category-and-subcategory', getProductsByCategoryAndSubcategoryController);
 productRouter.post('/get-by-id', getProductByIdController);
 productRouter.put('/update', auth, admin, updateProductController);
+productRouter.delete('/delete', auth, admin, deleteProductController);
 
 export default productRouter;
