d2cf8ae926422d3d410550620c94a9ddd9499877
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-12 23:59:55 +0530

    Add cart item qty update route

--- server/routes/cart.route.js
+++ server/routes/cart.route.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import auth from '../middlewares/auth.js';
 import {
     addToCartController,
-    getCartItemController
+    getCartItemController,
+    updateCartItemQtyController
 } from "../controllers/cart.controller.js";
 
 const cartRouter = new Router();
 
 cartRouter.post('/add', auth, addToCartController);
 cartRouter.get('/get', auth, getCartItemController);
+cartRouter.put('/update-qty', auth, updateCartItemQtyController);
 
 export default cartRouter;
