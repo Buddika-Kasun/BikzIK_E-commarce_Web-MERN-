5e19c42f4887f4f2646365ca1bedbf30b1406fe4
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-12 19:54:45 +0530

    Add item add to cart controller

---
+++ server/controllers/cart.controller.js
@@ -0,0 +1,61 @@
+import CartProductModel from "../models/cartProduct.model.js";
+import UserModel from "../models/user.model.js";
+
+
+// Add to cart controller
+export const addToCartController = async(req, res) => {
+    try {
+
+        const userId = req.userId;
+        const { productId } = req.body;
+
+        if(!productId) {
+            return res.status(400).json({
+                message: "Please provide productId",
+                error: true,
+                success: false,
+            });
+        }
+
+        const checkItemCart = await CartProductModel.findOne({
+            userId: userId,
+            productId: productId,
+        });
+
+        if(checkItemCart) {
+            return res.status(400).json({
+                message: "Product already exists in the cart",
+                error: true,
+                success: false,
+            });
+        }
+
+        const cartItem = new CartProductModel({
+            userId: userId,
+            productId: productId,
+            quantity: 1,
+        });
+
+        const save = await cartItem.save();
+
+        const updateUserCart = await UserModel.updateOne(
+            { _id: userId },
+            { $push: { shopping_cart: save._id } },
+        );
+
+        return res.json({
+            message: "Product added to cart successfully",
+            error: false,
+            success: true,
+            data: save,
+        });
+
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: err.message || err,
+            error: true,
+            success: false,
+        });
+    }
+};
