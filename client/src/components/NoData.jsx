14c5ec4da2fb6393c49afe037c38230399fac512
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-12 18:18:02 +0530

    Update no data component styles

--- client/src/components/NoData.jsx
+++ client/src/components/NoData.jsx
@@ -2,9 +2,9 @@ import { RiFileForbidLine } from "react-icons/ri";
 
 const NoData = () => {
     return (
-        <div className="flex flex-col items-center gap-4 p-8">
+        <div className="flex flex-col items-center gap-4 p-8 text-green-500">
             <RiFileForbidLine size={80} />
-            <p>Data not found!</p>
+            <p className="font-semibold">Data not found!</p>
         </div>
     );
 }
