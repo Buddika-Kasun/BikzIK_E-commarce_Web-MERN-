4de48b3fd3022ffe9bb57628933e8fc104438376
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-12 21:44:51 +0530

    Add get cart item route

--- server/routes/cart.route.js
+++ server/routes/cart.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import auth from '../middlewares/auth.js';
 import {
-    addToCartController
+    addToCartController,
+    getCartItemController
 } from "../controllers/cart.controller.js";
 
 const cartRouter = new Router();
 
 cartRouter.post('/add', auth, addToCartController);
+cartRouter.get('/get', auth, getCartItemController);
 
 export default cartRouter;
