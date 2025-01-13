dfb1e758ad42f9b0b0f43101ff257506224ae641
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-13 11:58:28 +0530

    Add cart item delete route

--- server/routes/cart.route.js
+++ server/routes/cart.route.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import auth from '../middlewares/auth.js';
 import {
     addToCartController,
+    deleteCartItemController,
     getCartItemController,
     updateCartItemQtyController
 } from "../controllers/cart.controller.js";
@@ -11,5 +12,6 @@ const cartRouter = new Router();
 cartRouter.post('/add', auth, addToCartController);
 cartRouter.get('/get', auth, getCartItemController);
 cartRouter.put('/update-qty', auth, updateCartItemQtyController);
+cartRouter.delete('/delete', auth, deleteCartItemController);
 
 export default cartRouter;
