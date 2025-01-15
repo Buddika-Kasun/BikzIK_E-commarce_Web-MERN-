cc0041d01e696f14eb97777bd0dd12a89e742a58
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-15 14:31:41 +0530

    Add address update route

--- server/routes/address.route.js
+++ server/routes/address.route.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import auth from "../middlewares/auth.js";
 import {
     addAddressController,
-    getAddressesController
+    getAddressesController,
+    updateAddressController
 } from "../controllers/address.controller.js";
 
 const addressRouter = new Router();
 
 addressRouter.post('/add', auth, addAddressController);
 addressRouter.get('/get', auth, getAddressesController);
+addressRouter.put('/update', auth, updateAddressController);
 
 export default addressRouter;
