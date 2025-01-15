9876cfa06f2ca1a6aa1893d8cb49186a18db473f
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-15 11:39:55 +0530

    Address slice

---
+++ client/src/store/addressSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    addressList: [],
+};
+
+const  addressSlice = createSlice({
+    name: "address",
+    initialState,
+    reducers: {
+        setAddressList: (state, action) => {
+            return {...state, addressList: [...action.payload]};
+        },
+    }
+});
+
+export const { setAddressList } = addressSlice.actions;
+
+export default addressSlice.reducer;
