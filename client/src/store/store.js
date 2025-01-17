c37d6f1753455fed6bed3b3330b1a14608609465
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-17 13:53:44 +0530

    Add order slice to store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,6 +3,7 @@ import userReducer from './userSlice';
 import productReducer from './productSlice';
 import cartReducer from './cartSlice';
 import addressReducer from './addressSlice';
+import orderReducer from './orderSlice';
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
     product: productReducer,
     cart: cartReducer,
     address: addressReducer,
+    order: orderReducer
   },
 })
