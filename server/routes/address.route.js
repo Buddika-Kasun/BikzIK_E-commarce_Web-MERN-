06ac473d11cc9c4b8fbc588272d7bfd4b4e3e3d5
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-17 13:55:24 +0530

    Add address get by id route

--- server/routes/address.route.js
+++ server/routes/address.route.js
@@ -3,6 +3,7 @@ import auth from "../middlewares/auth.js";
 import {
     addAddressController,
     deleteAddressController,
+    getAddressByIdController,
     getAddressesController,
     updateAddressController
 } from "../controllers/address.controller.js";
@@ -13,5 +14,6 @@ addressRouter.post('/add', auth, addAddressController);
 addressRouter.get('/get', auth, getAddressesController);
 addressRouter.put('/update', auth, updateAddressController);
 addressRouter.delete('/delete', auth, deleteAddressController);
+addressRouter.post('/get-by-id', getAddressByIdController);
 
 export default addressRouter;
