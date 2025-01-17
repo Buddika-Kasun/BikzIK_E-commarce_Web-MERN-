54707066c114f93390b5344bd81c42c86164e930
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-17 09:46:32 +0530

    Add order router to server

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ import subCategoryRouter from './routes/subCategory.route.js';
 import productRouter from './routes/product.route.js';
 import cartRouter from './routes/cart.route.js';
 import addressRouter from './routes/address.route.js';
+import orderRouter from './routes/order.route.js';
 
 dotenv.config();
 
@@ -49,6 +50,7 @@ app.use('/api/subcategory', subCategoryRouter);
 app.use('/api/product', productRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/address', addressRouter);
+app.use('/api/order', orderRouter);
 
 // Connect to MongoDB
 connectDB().then(() => {
