ced9d6a0a96667920ae8c31c4dd6639c9981c75c
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-17 20:02:07 +0530

    Add all order get route

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -2,13 +2,15 @@ import { Router } from 'express';
 import auth from '../middlewares/auth.js';
 import {
     addOrderController,
+    getAllOrdersController,
     getOrdersController
 } from '../controllers/order.controller.js';
-import { get } from 'mongoose';
+import { admin } from '../middlewares/admin.js';
 
 const orderRouter = new Router();
 
 orderRouter.post('/add', auth, addOrderController);
 orderRouter.get('/get', auth, getOrdersController);
+orderRouter.get('/get-all', auth, admin, getAllOrdersController);
 
 export default orderRouter;
