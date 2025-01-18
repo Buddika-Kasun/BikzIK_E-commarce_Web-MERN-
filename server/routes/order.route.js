2946623fb168de90138791c255398d68ccf42022
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-18 19:55:18 +0530

    Add order status update route

--- server/routes/order.route.js
+++ server/routes/order.route.js
@@ -3,7 +3,8 @@ import auth from '../middlewares/auth.js';
 import {
     addOrderController,
     getAllOrdersController,
-    getOrdersController
+    getOrdersController,
+    updateOrderStatusController
 } from '../controllers/order.controller.js';
 import { admin } from '../middlewares/admin.js';
 
@@ -12,5 +13,6 @@ const orderRouter = new Router();
 orderRouter.post('/add', auth, addOrderController);
 orderRouter.get('/get', auth, getOrdersController);
 orderRouter.get('/get-all', auth, admin, getAllOrdersController);
+orderRouter.put('/update-status', auth, admin, updateOrderStatusController);
 
 export default orderRouter;
