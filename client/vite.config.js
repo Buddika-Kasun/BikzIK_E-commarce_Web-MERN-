5a24135664fbdba813eefc121de2f9823f29168e
Buddika Kasun <buddikakasun80@gmail.com>
2025-01-20 11:13:07 +0530

    Test update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,4 @@
+/* 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,3 +6,23 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+ */
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    historyApiFallback: true, // Redirect all unknown routes to index.html
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+});
